3205be57dffaeba779214080a82ded0f8a68ad97
mannea94 <marjan.andonov@gmail.com>
2017-12-01 20:00:13 +0100

    Initial commit

---
+++ app/src/main/java/com/example/manne/projectdecember/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.manne.projectdecember;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.buttonLOG)
+    Button buttonLOG;
+    @BindView(R.id.textGuest)
+    TextView textGuest;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+}
